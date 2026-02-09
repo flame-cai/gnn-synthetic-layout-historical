25e8ab545f61ea94196e6242167cb737f8fdc8ef
kartikchincholikar <kartik.chincholikar@flame.edu.in>
2026-02-09 09:33:22 +0530

    deleted files

--- downstream_eval/commands.sh
+++
@@ -1,7 +0,0 @@
-# JSON
-python no_structure_prediction.py complex/images
-
-# PAGE-XML: do the structured predictions throught the app
-
-# PAGE-CML: predict using easy ofce
-python recognize_manuscript_text.py complex_structure
